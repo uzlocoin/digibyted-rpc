8cc26e29870b2ae7762925d579dd653c724bb8eb
digicontributer <esotericizm88@gmail.com>
2017-12-15 23:45:34 +1000

    digibyte

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-bitcoind-rpc.js
+digibyted-rpc.js
 ===============
 
 [![NPM Package](https://img.shields.io/npm/v/bitcoind-rpc.svg?style=flat-square)](https://www.npmjs.org/package/bitcoind-rpc)
@@ -9,10 +9,10 @@ A client library to connect to Bitcoin Core RPC in JavaScript.
 
 ## Get Started
 
-bitcoind-rpc.js runs on [node](http://nodejs.org/), and can be installed via [npm](https://npmjs.org/):
+digibyted-rpc.js runs on [node](http://nodejs.org/), and can be installed via [npm](https://npmjs.org/):
 
 ```bash
-npm install bitcoind-rpc
+npm install digibyted-rpc
 ```
 
 ## Examples
@@ -20,14 +20,14 @@ npm install bitcoind-rpc
 ```javascript
 var run = function() {
   var bitcore = require('bitcore');
-  var RpcClient = require('bitcoind-rpc');
+  var RpcClient = require('digibyted-rpc');
 
   var config = {
     protocol: 'http',
     user: 'user',
     pass: 'pass',
     host: '127.0.0.1',
-    port: '18332',
+    port: '12024',
   };
 
   var rpc = new RpcClient(config);
--- test/index.js
+++ test/index.js
@@ -75,7 +75,7 @@ describe('RpcClient', function() {
     var client = new RpcClient({
       user: 'user',
       pass: 'pass',
-      port: 8332,
+      port: 12024,
     });
     client.protocol.should.equal(https);
 
@@ -87,7 +87,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       protocol: 'http'
     });
     client.protocol.should.equal(http);
@@ -100,7 +100,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: true
     });
@@ -131,7 +131,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: false
     });
@@ -167,7 +167,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: false
     });
@@ -204,7 +204,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: false
     });
@@ -243,7 +243,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: true
     });
@@ -261,7 +261,7 @@ describe('RpcClient', function() {
     client.getBalance('n28S35tqEMbt6vNad7A5K3mZ7vdn8dZ86X', 6, function(error, parsedBuf) {
       requestStub.restore();
       should.exist(error);
-      error.message.should.equal('Bitcoin JSON-RPC: Connection Rejected: 401 Unnauthorized');
+      error.message.should.equal('DigiByte JSON-RPC: Connection Rejected: 401 Unnauthorized');
       done();
     });
 
@@ -273,7 +273,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: true
     });
@@ -291,7 +291,7 @@ describe('RpcClient', function() {
     client.getDifficulty(function(error, parsedBuf) {
       requestStub.restore();
       should.exist(error);
-      error.message.should.equal('Bitcoin JSON-RPC: Connection Rejected: 403 Forbidden');
+      error.message.should.equal('DigiByte JSON-RPC: Connection Rejected: 403 Forbidden');
       done();
     });
 
@@ -303,7 +303,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: true
     });
@@ -322,7 +322,7 @@ describe('RpcClient', function() {
     client.getDifficulty(function(error, parsedBuf) {
       requestStub.restore();
       should.exist(error);
-      error.message.should.equal('Bitcoin JSON-RPC: Work queue depth exceeded');
+      error.message.should.equal('DigiByte JSON-RPC: Work queue depth exceeded');
       done();
     });
 
@@ -334,7 +334,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: true
     });
@@ -356,7 +356,7 @@ describe('RpcClient', function() {
     client.getDifficulty(function(error, parsedBuf) {
       requestStub.restore();
       should.exist(error);
-      error.message.should.equal('Bitcoin JSON-RPC: Request Error: write EPIPE');
+      error.message.should.equal('DigiByte JSON-RPC: Request Error: write EPIPE');
       done();
     });
 
@@ -368,7 +368,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: true
     });
@@ -401,7 +401,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: true
     });
@@ -419,7 +419,7 @@ describe('RpcClient', function() {
     client.getDifficulty(function(error, parsedBuf) {
       requestStub.restore();
       should.exist(error);
-      error.message.should.equal('Bitcoin JSON-RPC: Request Error: connect ECONNREFUSED');
+      error.message.should.equal('DigiByte JSON-RPC: Request Error: connect ECONNREFUSED');
       done();
     });
 
@@ -431,7 +431,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: true
     });
@@ -450,7 +450,7 @@ describe('RpcClient', function() {
     client.getDifficulty(function(error, parsedBuf) {
       requestStub.restore();
       should.exist(error);
-      error.message.should.equal('Bitcoin JSON-RPC: Error Parsing JSON: Unexpected token o');
+      error.message.should.equal('DigiByte JSON-RPC: Error Parsing JSON: Unexpected token o');
       done();
     });
 
@@ -462,7 +462,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: true
     });
@@ -481,7 +481,7 @@ describe('RpcClient', function() {
     client.getDifficulty(function(error, parsedBuf) {
       requestStub.restore();
       should.exist(error);
-      error.message.should.equal('Bitcoin JSON-RPC: Error Parsing JSON: Unexpected end of input');
+      error.message.should.equal('DigiByte JSON-RPC: Error Parsing JSON: Unexpected end of input');
       done();
     });
 
@@ -493,7 +493,7 @@ describe('RpcClient', function() {
       user: 'user',
       pass: 'pass',
       host: 'localhost',
-      port: 8332,
+      port: 12024,
       rejectUnauthorized: true,
       disableAgent: true
     });
